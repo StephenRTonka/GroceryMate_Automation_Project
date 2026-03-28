60002983a2675146d82e363bbf17737ccb0e021b
StephenRTonka <wildenbruchplatz@gmail.com>
2026-03-28 13:33:24 +0100

    Add utils

---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+class Config:
+    AUTH_URL = "https://grocerymate.masterschool.com/auth"
+    VALID_USERNAME = "johndoe@example.com"
+    VALID_PASSWORD = "admin123"
+    INVALID_PASSWORD = "jdhdhd"
+    ERROR_MESSAGE = "Invalid username or password"
